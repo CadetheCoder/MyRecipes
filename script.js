2e296c60b54909aba55d83b9054aa84ee12d1ac2
CadetheCoder <cadehultz06@gmail.com>
2025-03-09 17:22:42 +0900

    Small Changes

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const apiKey = "1473ad9c323c4f38aa9e70152e87f337";
-const searchInput = document.getElementById("search");
+const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
-
-searchBtn.addEventListener("click", () => {
-    const query = searchInput.value.trim();
-
-})
+const content1Img = document.getElementById("content1-img");
+const content1Title = document.getElementById("content1-title");
 
 const fetchRecipes = (query) => {
     fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`)
@@ -13,4 +10,14 @@ const fetchRecipes = (query) => {
     .then(data => {})
 }
 
+searchInput.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        const query = searchInput.value.trim();
+
+        if (query) {
+            window.location.href = `Search Results.html?query=${encodeURIComponent(query)}`;
+        }
+    }
+
+})
 
