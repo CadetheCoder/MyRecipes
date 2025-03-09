58f8496da94cb1fccace67dff11c433942480a72
CadetheCoder <cadehultz06@gmail.com>
2025-03-09 19:35:35 +0900

    Trying to fix search results JS

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const apiKey = "1473ad9c323c4f38aa9e70152e87f337";
 const searchInput = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
 const content1Img = document.getElementById("content1-img");
 const content1Title = document.getElementById("content1-title");
-
-const fetchRecipes = (query) => {
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`)
-    .then(rep => rep.json())
-    .then(data => {})
-}
 
 searchInput.addEventListener("keydown", e => {
     if (e.key === "Enter") {
         const query = searchInput.value.trim();
 
         if (query) {
-            window.location.href = `Search Results.html?query=${encodeURIComponent(query)}`;
+            window.location.href = `search-results.html?query=${encodeURIComponent(query)}`;
         }
     }
 
 })
+
+/* const fetchRecipes = (query) => {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`)
+    .then(rep => rep.json())
+    .then(data => {})
+}
+ */
+
 
