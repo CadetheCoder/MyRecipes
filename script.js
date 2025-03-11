1ccc8d074873d669f9594a0d0ba9a5cdc8e0cd2f
CadetheCoder <cadehultz06@gmail.com>
2025-03-11 12:51:56 +0900

    File changes

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const apiKey = "1473ad9c323c4f38aa9e70152e87f337";
-
 const searchInput = document.getElementById("search-input");
-
-const searchResultsCard = document.getElementById("search-results-card");
 
 searchInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
@@ -19,27 +16,5 @@ searchInput.addEventListener("keydown", (e) => {
     }
 });
 
-const fetchRecipes = (query) => {
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`)
-        .then(response => response.json())
-        .then(data => {
-            displayResults(data.results);
-        })
-        .catch(error => console.error("Error fetching request", error));
-}
 
-const displayResults = (recipes) => {
-    if (recipes.length === 0) {
-        searchResultsCard.innerHTML = "<p>No recipes found</p>";
-        return;
-    }
-}
-
-recipes.forEach(recipe => {
-    const recipeElement = document.createElement("div");
-    recipeElement.innerHTML = `
-    <img src="${recipe.image} alt="${recipe.title}" width="200">
-    <p>${recipe.title}</p>`;
-    searchResultsCard.appendChild(recipeElement);
-}) 
     
